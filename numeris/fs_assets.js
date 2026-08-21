8a507c6ea040f67c84ed82117b9f1060177ed50c
maeiky <maeiky@gmail.com>
2026-08-20 21:46:59 -0400

    #

--- numeris/fs_assets.js
+++ numeris/fs_assets.js
@@ -37,6 +37,40 @@
     return path + sep + "_v=" + encodeURIComponent(v) + "&_t=" + t;
   }
 
+  function fnv1a32(bytes) {
+    var hash = 0x811c9dc5, hex;
+    for (var i = 0; i < bytes.length; i++) {
+      hash ^= bytes[i];
+      hash = Math.imul(hash, 0x01000193);
+    }
+    hex = (hash >>> 0).toString(16);
+    return "00000000".slice(hex.length) + hex;
+  }
+
+  function logSdfFontDiag(path, bytes) {
+    var doc, glyphs, keys, gKey = "", i;
+    if (path !== "Rc/font/legacy_clean.sdffont.json") return;
+    try {
+      doc = JSON.parse(new TextDecoder("utf-8").decode(bytes));
+      glyphs = doc && doc.glyphs ? doc.glyphs : {};
+      keys = Object.keys(glyphs);
+      for (i = 0; i < keys.length; i++) {
+        if ((glyphs[keys[i]].codepoint | 0) === 71) {
+          gKey = keys[i];
+          break;
+        }
+      }
+      console.info("[fs_assets][sdf-diag] bytes=" + bytes.byteLength +
+        " fnv1a32=" + fnv1a32(bytes) +
+        " declared=" + (doc.glyph_count | 0) +
+        " nodes=" + keys.length +
+        " U+0047=" + (gKey || "MISSING"));
+    } catch (err) {
+      console.error("[fs_assets][sdf-diag] parse failed bytes=" + bytes.byteLength +
+        " fnv1a32=" + fnv1a32(bytes), err);
+    }
+  }
+
   function loadOne(path) {
     var dep = "numeris_asset:" + path;
     addRunDependency(dep);
@@ -44,10 +78,12 @@
       if (!res.ok) throw new Error(path + " HTTP " + res.status);
       return res.arrayBuffer();
     }).then(function (buf) {
+      var bytes = new Uint8Array(buf);
       ensureDir(path);
-      FS.writeFile(path, new Uint8Array(buf));
+      FS.writeFile(path, bytes);
       if (typeof console !== "undefined" && console.info)
         console.info("[fs_assets] mounted", path, "(" + buf.byteLength + " bytes)");
+      logSdfFontDiag(path, bytes);
       removeRunDependency(dep);
     }).catch(function (err) {
       if (typeof console !== "undefined" && console.error)
